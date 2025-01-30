8cf8fdf469b156ccdcfafc8c5e7c6d3b8b38d05c
Maestro1982 <kris.tote@telenet.be>
2025-01-30 15:49:18 +0100

    Implement Greetings and MiniCard in the Home page

---
+++ pos-frontend-react/src/components/home/Greetings.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+
+const Greetings = () => {
+  const [dateTime, setDateTime] = useState<Date>(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setDateTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatDate = (date: Date): string => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    return `${months[date.getMonth()]} ${String(date.getDate()).padStart(
+      2,
+      '0'
+    )}, ${date.getFullYear()}`;
+  };
+
+  const formatTime = (date: Date): string => {
+    return `${String(date.getHours()).padStart(2, '0')}:${String(
+      date.getMinutes()
+    ).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+  };
+
+  return (
+    <div className='flex items-center justify-between px-8 mt-5'>
+      <div>
+        <h1 className='text-[#f5f5f5] text-2xl font-semibold tracking-wide'>
+          Good morning, John
+        </h1>
+        <p className='text-[#ababab] text-sm'>
+          Give your best services for our customers 😊
+        </p>
+      </div>
+      <div className='flex flex-col items-start'>
+        <span className='text-[#f5f5f5] text-3xl font-bold tracking-wide w-[8rem]'>
+          {formatTime(dateTime)}
+        </span>
+        <span className='text-[#ababab] text-sm'>{formatDate(dateTime)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Greetings;
--- pos-frontend-react/src/pages/Home.tsx
+++ pos-frontend-react/src/pages/Home.tsx
@@ -1,14 +1,36 @@
+import { TbReceiptEuro } from 'react-icons/tb';
+import { GiSandsOfTime } from 'react-icons/gi';
+
+import Greetings from '../components/home/Greetings';
+import MiniCard from '../components/home/MiniCard';
 import BottomNav from '../components/shared/BottomNav';
 
 const Home = () => {
   return (
     <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
       {/* Left Side */}
-      <div className='flex-[3] bg-[#1a1a1a]'></div>
+      <div className='flex-[3]'>
+        <Greetings />
+        <div className='flex items-center w-full gap-3 px-8 mt-8'>
+          <MiniCard
+            title='Total Earnings'
+            icon={TbReceiptEuro}
+            number={512}
+            footerNum={1.6}
+          />
+          <MiniCard
+            title='In Progress'
+            icon={GiSandsOfTime}
+            number={16}
+            footerNum={3.6}
+          />
+        </div>
+      </div>
       {/* Right Side */}
       <div className='flex-[2] bg-[#1a1a1a]'></div>
       <BottomNav />
     </section>
   );
 };
+
 export default Home;
